b8a2b2e692ce76bd445c144255bb5428cfb09a0a
John Lee <johndlee2009@gmail.com>
2022-10-01 22:01:37 -0400

    resolved2

--- map.py
+++ map.py
@@ -4,7 +4,7 @@ import numpy as np
 
 frames = sorted(os.listdir('im_folder'))
 
-im_map = ','.join(['im_folder/' + frames[j] for j,i in enumerate(np.linspace(0, 90, len(frames)))])
+im_map = ['im_folder/' + frames[j] for j,i in enumerate(np.linspace(0, 90, len(frames)))]
 
-with open("map.txt", 'w') as f:
+with open("map.json", 'w') as f:
     json.dump(im_map,f, sort_keys=True)
